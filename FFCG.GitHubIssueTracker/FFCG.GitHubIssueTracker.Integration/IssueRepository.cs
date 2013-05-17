5c0e5e01feb78c0a15dc424063f7665d8e3df191
FredrikGoransson <fredrik.goransson@gmail.com>
2013-05-17 11:16:13 +0200

    Added IssueRepository and tests

--- FFCG.GitHubIssueTracker/FFCG.GitHubIssueTracker.Integration/IssueRepository.cs
+++ FFCG.GitHubIssueTracker/FFCG.GitHubIssueTracker.Integration/IssueRepository.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using FFCG.GitHubIssueTracker.Integration.Models;
 
 namespace FFCG.GitHubIssueTracker.Integration
 {
     public class IssueRepository
     {
+        public string AuthorizationToken { get; set; }
+
+        public IssueRepository(string authorizationToken)
+        {
+            AuthorizationToken = authorizationToken;
+        }
+
         public IEnumerable<Issue> GetAllIssues()
         {
+            // C:\Code> curl -i -H 'Authorization: token 8768769532a411f9d7ffdb7a8804af6fae7bad0e' https://api.github.com/repos/ffcg/GitHubIssueTracker/issues
             return new List<Issue>();
-        } 
+        }
+
+        public Issue CreateIssue(string title, string description, string assignee)
+        {
+            var issue = new Issue() {Title = title, Description = description, Assignee = assignee};    
+            return issue;
+        }
     }
 }
---
+++ FFCG.GitHubIssueTracker/FFCG.GitHubIssueTracker.Web.Tests/Integration/IssueRepositoryTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using FFCG.GitHubIssueTracker.Integration;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FFCG.GitHubIssueTracker.Web.Controllers;
+using NUnit.Framework;
+
+namespace FFCG.GitHubIssueTracker.Web.Tests.Integration
+{
+    public class IssueRepositoryTest
+    {
+        // ReSharper disable InconsistentNaming
+
+        private string authorizationToken = "8768769532a411f9d7ffdb7a8804af6fae7bad0e";
+
+        [Test]
+        public void CreateIssue_should_create_an_Issue()
+        {
+            // Arrange
+            var repository = new IssueRepository(authorizationToken: authorizationToken);
+
+            // Act
+            var issue = repository.CreateIssue(
+                title:"Test issue " + Guid.NewGuid().ToString(), 
+                description:"This is an issue created by a test, it can safely be ignored", 
+                assignee:"FredrikGoransson");
+
+            // Assert
+            issue.Should().NotBeNull();
+        }
+
+        [Test]
+        public void CreateIssue_should_create_an_Issue_with_Id_assigned_by_GitHub()
+        {
+            // Arrange
+            var repository = new IssueRepository(authorizationToken: authorizationToken);
+
+            // Act
+            var issue = repository.CreateIssue(
+                title: "Test issue " + Guid.NewGuid().ToString(),
+                description: "This is an issue created by a test, it can safely be ignored",
+                assignee: "FredrikGoransson");
+
+            // Assert
+            issue.Id.Should().NotBeNullOrEmpty("GitHub should control creation of Id's");
+        }
+
+        [Test]
+        public void CreateIssue_should_add_an_Issue_to_GitHub()
+        {
+            // Arrange
+            var repository = new IssueRepository(authorizationToken: authorizationToken);
+
+            // Act
+            var issue = repository.CreateIssue(
+                title: "Test issue " + Guid.NewGuid().ToString(),
+                description: "This is an issue created by a test, it can safely be ignored",
+                assignee: "FredrikGoransson");
+            var retrievedIssue = repository.GetAllIssues().FirstOrDefault(i => i.Id == issue.Id);
+
+            // Assert
+            retrievedIssue.Should().NotBeNull("GitHub should store the Issue");
+        }
+
+        [Test]
+        public void GetAllIssues_should_not_return_empty_list()
+        {
+            // Arrange
+            var repository = new IssueRepository(authorizationToken: authorizationToken);
+            repository.CreateIssue("Test issue " + Guid.NewGuid().ToString(), "This is an issue created by a test, it can safely be ignored", "FredrikGoransson");
+
+            // Act
+            var result = repository.GetAllIssues();
+
+            // Assert
+            result.Should().NotBeEmpty("GetAllIssues should return a non empty list");
+        }
+
+        // ReSharper restore InconsistentNaming
+    }
+}
